a885faaa9d5e1fcbade7c45e606e801efa1272e7
MAlexa <aryalexa@gmail.com>
2025-08-28 18:42:13 +0200

    feat: new tab in webpage, images included

--- test/test_example.cpp
+++
@@ -1,9 +0,0 @@
-#include <cassert>
-
-int main() {
-    ConfigParser parser;
-    assert(parser.load("test.conf") == true);
-    assert(parser.getValue("server", "listen") == "8080");
-    return 0;
-}
-
